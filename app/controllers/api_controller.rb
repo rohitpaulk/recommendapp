69c19e4b6c81b33ef0d3a20b70fe317bd257f3b3
Rohit Paul Kuruvilla <rohitpaulk@live.com>
2015-01-18 20:13:50 +0530

    Conditional Check

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,15 +3,19 @@ class ApiController < ApplicationController
 
 	def app_callback
 		user_params = params.permit(:uid, :name, :access_token, :email)
-		user = User.new
-		user.email = user_params[:email]
-		user.name = user_params[:name]
-		user.elsewheres.build(
-			:provider => 'facebook',
-			:uid => user_params[:uid],
-			:access_token => user_params[:access_token]
-		)
-		user.save
+		if Elsewhere.exists?(:uid => user_params[:uid], :provider => 'facebook')
+			user = Elsewhere.where(:uid => user_params[:uid], :provider => 'facebook').first.user
+		else
+			user = User.new
+			user.email = user_params[:email]
+			user.name = user_params[:name]
+			user.elsewheres.build(
+				:provider => 'facebook',
+				:uid => user_params[:uid],
+				:access_token => user_params[:access_token]
+			)
+			user.save
+		end
 		render :json => {
 			"user" => user,
 			"elsewhere" => user.elsewheres.first
