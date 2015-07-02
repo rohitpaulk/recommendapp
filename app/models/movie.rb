87a53d42c5f4a602893b05cd3a022bb83645d1b1
aandis <abhishekrm@iitrpr.ac.in>
2015-07-02 23:49:37 +0530

    Replaced omdb with tmdb.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -9,23 +9,42 @@ class Movie < ActiveRecord::Base
     if Movie.exists?(title: title)
       Movie.find_by_title(title)
     else
-      response = Omdb::Api.new.fetch(title)[:movie]
-      return nil unless response
+      Enceladus.connect("eace344fe11061cf0a80c99ddd40c34a",
+        {
+          include_image_language: "en",
+          language: "en,null",
+          include_adult: true
+        })
+      Enceladus::Configuration::Image.instance.setup!
+      movie = Enceladus::Movie.find_by_title(title).first
+      return nil unless movie
       movie = Movie.create!(
-        title:       response.title,
-        year:        response.year,
-        plot:        response.plot,
-        imdb_id:     response.imdb_id,
-        imdb_rating: response.imdb_rating,
-        poster_url:  response.poster
+        title:       movie.original_title,
+        year:        movie.release_date,
+        plot:        movie.overview,
+        imdb_id:     movie.id,  #This isn't imdb id. TODO
+        imdb_rating: movie.vote_average,  #Not imdb rating. TODO
+        poster_url:  Enceladus::Configuration::Image.instance.url_for(
+          "logo", movie.poster_path)[-1] #TODO
       )
     end
   end
 
   def self.popular_movies
+    Enceladus.connect("eace344fe11061cf0a80c99ddd40c34a",
+      {
+        include_image_language: "en",
+        language: "en,null",
+        include_adult: true
+      })
+    Enceladus::Configuration::Image.instance.setup!
+
+    collection = Enceladus::Movie.popular
+    movies = collection.results_per_page[0]
     Enumerator.new do |e|
-      ['Terminator', 'Interstellar', 'Dark Knight', 'Titanic', 'Spiderman'].each { |title|
-        e.yield Movie.from_title(title)
+      movies.each { |movie|
+        puts movie.original_title
+        e.yield movie
       }
     end
   end
