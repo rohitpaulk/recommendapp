4a09142e3249d68a66cfc038cdb4e88f4057eb2e
aandis <abhishekrm@iitrpr.ac.in>
2015-08-15 16:02:56 +0530

    Added tests.

--- app/controllers/api/friends_controller.rb
+++ app/controllers/api/friends_controller.rb
@@ -11,22 +11,22 @@ class Api::FriendsController < ApplicationController
 
     # If item_type exists, should be valid
     unless [nil, 'Movie', 'AndroidApp'].include?(params[:item_type])
-      render plain: "Invalid item_type, should be AndroidApp", status: 422 and return
+      render plain: "Invalid item_type", status: 422 and return
     end
 
     result = user.following
 
     if params[:item_type] && params[:item_id]
       result = result.each.map do |follower|
-        has_item = !follower.user_items.where(
+        has_item = follower.user_items.where(
                                   :item_type => params[:item_type], 
                                   item_id: params[:item_id]
-                                ).empty?
-        has_been_recommended = !user.recommendations.where(
+                                ).exists?
+        has_been_recommended = user.recommendations.where(
                                   :recommendee => follower, 
                                   :item_type => params[:item_type], 
                                   :item_id => params[:item_id]
-                                ).empty?
+                                ).exists?
 
         follower = follower.serializable_hash
         follower[:has_item] = has_item
